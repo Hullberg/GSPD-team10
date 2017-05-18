b1ab8c50b28b4c3865a06c6ca9d9272960e2d439
Linh Tran <linhtt@ows.vn>
2017-05-18 18:17:26 +0700

    update webSocket server in python

--- testWebSocket.py
+++ testWebSocket.py
@@ -1,1 +1,25 @@
-import websocket
+# This is a simple WebSocket echo server
+# I took from this repo from https://github.com/dpallot/simple-websocket-server
+
+# I was using built-in python 2.7, Ubuntu 14.04
+# Just run this installation line:
+# sudo pip install git+https://github.com/dpallot/simple-websocket-server.git
+
+# And then run the GUI.html to see the test result :)
+
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+class SimpleEcho(WebSocket):
+
+    def handleMessage(self):
+        # echo message back to client
+        self.sendMessage(self.data)
+
+    def handleConnected(self):
+        print(self.address, 'connected')
+
+    def handleClose(self):
+        print(self.address, 'closed')
+
+server = SimpleWebSocketServer('', 9000, SimpleEcho)
+server.serveforever()
