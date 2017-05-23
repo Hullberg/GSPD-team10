2223287e9e57a1d3ad06db609d6d3921830efff7
Hullberg <johan.hullberg@gmail.com>
2017-05-23 21:08:23 +0200

    added to insert item and update slot, but need to pass args

--- testWebSocket.py
+++ testWebSocket.py
@@ -16,6 +16,7 @@ from tornado import ioloop, gen
 from tornado.ioloop import IOLoop
 import time
 from motor import motor_tornado
+import pprint
 
 mc_client = motor_tornado.MotorClient("mongodb://root:root@ds135798.mlab.com:35798/gspd",connectTimeoutMS=30000,socketTimeoutMS=None,socketKeepAlive=True)
 db = mc_client.gspd
@@ -31,19 +32,26 @@ class SimpleEcho(WebSocket):
 		
 		if command == 'store':
 			packageName = parameters[1]
-			tempMin = parameters[2]
-			tempMax = parameters[3]
-			lightMin = parameters[4]
-			lightMax = parameters[5]
+			tempMin = int(parameters[2])
+			tempMax = int(parameters[3])
+			lightMin = int(parameters[4])
+			lightMax = int(parameters[5])
+			# pprint(tempMin)
+			# pprint(tempMax)
+			# pprint(lightMin)
+			# pprint(lightMax)
 			
-
+			
 			client.sendMessage(u'Found a slot for ' + packageName)
 			client.sendMessage(packageName + u' is queued for storing. Please be informed');
 
 			# gte = greater than or equal to
 			# lte = lesser than or equal to
-			simpleQuery = { "lightSensitivity" : {"$lt" : 800}, "lightSensitivity" : {"$gt" : 700} }
+			simpleQuery = { "lightSensitivity" : {"$lt" : lightMax}, "lightSensitivity" : {"$gt" : lightMin}, "temperature" : {"$lt" : tempMax}, "temperature" : {"$gt" : tempMin}}
 
+			# # #
+			# TODO : MUST SEND parameters[1:5] TO CALLBACK
+			# # #
 			db.slot.find_one(simpleQuery, callback=getSlotDone)
 			ioloop.IOLoop.current().start()
 
@@ -64,12 +72,6 @@ class SimpleEcho(WebSocket):
 			packageId = parameters[1]
 			client.sendMessage(u'Package ' + packageId + ' is queued for delivery to the gate. Please be informed');
 
-			# get item and slot info
-			ioloop.IOLoop.current.start()
-			itemToGet = asyncdb.getItem({ "_id" : packageId})
-			ioloop.IOLoop.current.stop()
-			slotToGet = asyncdb.getItem({ "_id" : itemToGet[10]})
-			ioloop.IOLoop.current.stop()
 
 	def handleConnected(client):
 		print(client.address, 'connected')
@@ -78,10 +80,39 @@ class SimpleEcho(WebSocket):
 		print(client.address, 'closed')
 		
 
-def getSlotDone(result, error):
+def getSlotDone(result,error):
 	print('Slot info is now ready')
 	print('result %s error %s' % (repr(result), repr(error)))
-	IOLoop.current().stop()
+	slotID = repr(result['_id'])
+	xCoord = repr(result['xCoord'])
+	yCoord = repr(result['yCoord'])
+	#packageName = ??
+	#tempMin = ??
+	#tempMax = ??
+	#lightMin = ??
+	#lightMax = ??
+	# We also need the packageName, tempMin, tempMax, lightMin and lightMax of the item.. HOW?!
+
+
+	postItemQuery = { "itemName" : "test", "xCoord" : xCoord, "yCoord" : yCoord, "tempMin" : 5, "tempMax" : 10, "lightMin" : 700, "lightMax" : 800, "itemTaken" : False, "robotID" : None, "slotID" : slotID}
+	db.item.insert_one(postItemQuery, callback=postItemDone)
+
+def postItemDone(result,error):
+	print('Item inserted')
+	print('result %s error %s' % (repr(result), repr(error)))
+	itemID = repr(result.inserted_id) # The ID of the inserted item, will now update slot.
+	# Need the slotID from inserted item
+
+	updateSlotQuery = { "_id" : slotID , "$set" : { "itemID" : itemID } }
+
+	db.slot.update(updateSlotQuery, callback=updateSlotDone)
+
+
+def updateSlotDone(result,error):
+	print('Slot updated')
+	print('result %s error %s' % (repr(result),repr(error)))
+
+	ioloop.IOLoop.current().stop()
 
 
 if __name__ == '__main__':
